2bc70e6bc6e890e10826b09de2dcea4f9633e678
Yura Trambitskiy <tr.yura@yahoo.com>
2019-05-09 23:19:27 +0200

    Fix views blinking
    It appears that we don't need to setup the default name,
    updating of the name is enough

--- OpenSearchInNewTab.py
+++ OpenSearchInNewTab.py
@@ -15,39 +15,29 @@ class OpenSearchInNewTab(sublime_plugin.EventListener):
     # during new search
     def on_activated(self, view):
         if self.is_search_view(view):
-            self.apply_alt_name(view)
+            t = Timer(.001, self.update_view, (view,))
+            t.start()
 
     # these hooks will help other plugins
     # to understand that we are in search results file
     def on_text_command(self, view, command_name, args):
         if self.is_search_view(view):
-            self.apply_default_name(view)
-
-    def post_text_command(self, view, command_name, args):
-        if self.is_search_view(view):
-            self.apply_alt_name(view)
+            self.update_view(view)
 
     def get_alt_name(self, view):
         first_line_coords = view.full_line(sublime.Region(0, 0))
         first_line = view.substr(sublime.Region(*first_line_coords))
-        match = re.search('Searching \d+ files for "(.*)"$', first_line)
+        match = re.search('^Searching \d+ files for "(.*)"$', first_line)
 
         if match:
             query = match.group(1)
-            return ALT_NAME_BASE + '"' + query + '"'
+            return ALT_NAME_BASE + 'for "' + query + '"'
 
         return ALT_NAME_BASE
 
-    def apply_alt_name(self, view):
-        t = Timer(.1, self.apply_alt_name_async, (view,))
-        t.start()
-
-    def apply_alt_name_async(self, view):
+    def update_view(self, view):
         view.set_name(self.get_alt_name(view))
-
-    def apply_default_name(self, view):
-        view.set_name(DEFAULT_NAME)
-        self.apply_alt_name(view)
+        view.set_scratch(True)
 
     def is_search_view(self, view):
         name = view.name()
