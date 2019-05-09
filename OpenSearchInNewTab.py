5ad380bb99ffab4f7188ebf87cc25934737d91eb
Yura Trambitskiy <tr.yura@yahoo.com>
2019-05-09 22:52:02 +0200

    PoC of named buffer function

--- OpenSearchInNewTab.py
+++ OpenSearchInNewTab.py
@@ -1,8 +1,11 @@
-import sublime_plugin
+import re
 from threading import Timer
 
+import sublime_plugin
+import sublime
+
 DEFAULT_NAME = 'Find Results'
-ALT_NAME = 'Find Results '
+ALT_NAME_BASE = 'Find Results '
 
 
 class OpenSearchInNewTab(sublime_plugin.EventListener):
@@ -24,14 +27,36 @@ class OpenSearchInNewTab(sublime_plugin.EventListener):
         if self.is_search_view(view):
             self.apply_alt_name(view)
 
+    def get_alt_name(self, view):
+        first_line_coords = view.full_line(sublime.Region(0, 0))
+        first_line = view.substr(sublime.Region(*first_line_coords))
+        match = re.search('Searching \d+ files for "(.*)"$', first_line)
+
+        if match:
+            query = match.group(1)
+            return ALT_NAME_BASE + '"' + query + '"'
+
+        return ALT_NAME_BASE
+
     def apply_alt_name(self, view):
-        view.set_name(ALT_NAME)
+        t = Timer(.1, self.apply_alt_name_async, (view,))
+        t.start()
+
+    def apply_alt_name_async(self, view):
+        view.set_name(self.get_alt_name(view))
 
     def apply_default_name(self, view):
         view.set_name(DEFAULT_NAME)
-        t = Timer(.1, self.apply_alt_name, (view,))
-        t.start()
+        self.apply_alt_name(view)
 
     def is_search_view(self, view):
         name = view.name()
-        return name == ALT_NAME or name == DEFAULT_NAME
+
+        print('name ' + name)
+        print('alt_name ' + self.get_alt_name(view))
+
+        return (
+            name == DEFAULT_NAME or
+            name == ALT_NAME_BASE or
+            name == self.get_alt_name(view)
+            )
